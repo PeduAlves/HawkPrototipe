fe7000f462177f97b3ef0df525700119582f2185
Pedro Alves <62018086+PeduAlves@users.noreply.github.com>
2023-12-16 10:10:06 -0300

    Add IDamageable and DisableBullet
    Adicionando a interface de danificavel e o tempo para a bala ser desativada

--- Assets/Runtime/script/Bullet.cs
+++ Assets/Runtime/script/Bullet.cs
@@ -8,20 +8,20 @@ public class Bullet : MonoBehaviour, IPooledObject
     public float bulletForce = 20f;
     public Rigidbody rb;
     public void OnObjectSpawn(){
-        
-       rb.AddForce( 0f, 0f, 1f * bulletForce * Time.deltaTime);
 
+       StartCoroutine(DisableBullet());
     }
 
     private void FixedUpdate() {
 
         rb.velocity = new Vector3( 0f, 0f, 1f * bulletForce * Time.deltaTime);
-
     }
 
-    private void OnCollisionEnter( Collision other ){
+    private void OnCollisionEnter( Collision other ){}
 
-        
+    IEnumerator DisableBullet(){
+
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
     }
-
 }
--- Assets/Runtime/script/GameEvents.cs
+++ Assets/Runtime/script/GameEvents.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class GameEvents : MonoBehaviour
 {
   public static GameEvents Instance;
-
   private void Awake()=>Instance = this;
 
   public event Action EnemyTakeDamage;
+  
 }
