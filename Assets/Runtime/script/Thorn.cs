d3597e44b98b9a0dc225d3e2d25816ab462df6ea
Pedro Alves <62018086+PeduAlves@users.noreply.github.com>
2024-02-19 15:43:21 -0300

    Pequenas alterações

--- Assets/Runtime/script/Thorn.cs
+++ Assets/Runtime/script/Thorn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spines : MonoBehaviour
+public class Thorn : MonoBehaviour
 {
     public int damage = 10;
     private void OnTriggerEnter( Collider other ){
