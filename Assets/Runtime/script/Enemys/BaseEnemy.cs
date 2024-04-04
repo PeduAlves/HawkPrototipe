1d9186c0f4a5cfa9c6e7df7b4cb390827af4ee8f
Pedro Alves <62018086+PeduAlves@users.noreply.github.com>
2024-04-04 13:31:43 -0300

    Swaping EnemyScript

---
+++ Assets/Runtime/script/Enemys/BaseEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum enemyStates{
+
+    PATROL, FOLLOW, ATTACK
+}
+public class BaseEnemy : MonoBehaviour
+{
+
+    public Transform player;
+    
+
+}
