97a944e8157e95b708a3dd45cfb7188a5500c558
Pedro Alves <62018086+PeduAlves@users.noreply.github.com>
2024-08-01 12:56:17 -0300

    Corrigindo bug EnemyMerg
    Resolvendo os bugs do enemyMerg, que nao entrava nos estados follow e attack, por motivos desconhecidos, a solução foi pegar o inimigo base que estava funcionando, e apenas trocar o script colocando do enemyMerg, e agora está funcionando.

--- Assets/Runtime/script/Enemys/EnemyMerg.cs
+++ Assets/Runtime/script/Enemys/EnemyMerg.cs
@@ -5,44 +5,12 @@ using UnityEngine;
 
 public class EnemyMerg : BaseEnemy
 {
-    protected override void Update(){
+    protected override IEnumerator Attack(){
 
-        switch(state){
-            case enemyStates.DIE:
-                break;
+        isAttacking = true;
 
-            case enemyStates.PATROL:
-                if (playerInSight) state = enemyStates.FOLLOW;
-                else StartCoroutine(Patrol());
-            break;
+        yield return new WaitForSeconds(attackDelay);
 
-            case enemyStates.FOLLOW:
-                if(!playerInSight)state = enemyStates.FOLLOW;
-                else Follow();
-            break;
-
-            case enemyStates.ATTACK:
-                if(!playerInSight) state = enemyStates.FOLLOW;
-                if(PlayerInAttackRange() && !isAttacking) StartCoroutine(Attack());
-                else Follow();
-            break;
-        }
-    }
-    protected override void Follow()
-    {
-        if(PlayerInAttackRange()){ 
-            
-            state = enemyStates.ATTACK;
-            return;
-        }
-        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
-        transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
-    
-    }
-
-    protected override IEnumerator Attack()
-    {
-        yield return base.Attack();
-        print ("Attacking");
+        isAttacking = false;
     }
 }
