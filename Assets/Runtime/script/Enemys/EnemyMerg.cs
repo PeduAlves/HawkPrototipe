247070e72f2359b69077fa4e9b36635689a6edea
Pedro Alves <62018086+PeduAlves@users.noreply.github.com>
2024-08-14 11:23:32 -0300

    Removendo os UnityEngine nos vector3
    Removendo o problema que fazia com que todos os vector3 fossem usados com o unityEngine, o problema era por causa do system.numerics, que tambem tem um vector3 e estava dando conflito

--- Assets/Runtime/script/Enemys/EnemyMerg.cs
+++ Assets/Runtime/script/Enemys/EnemyMerg.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Numerics;
 using UnityEngine;
 public class EnemyMerg : BaseEnemy
 {
@@ -10,29 +9,26 @@ public class EnemyMerg : BaseEnemy
     public float mergAttackDelay = 0.5f;
     public GameObject enemySigth;
 
-    //Por algum motivo não aparente, os vector3, so estão funcionando com o UnityEngine.Vector3
-
     protected override IEnumerator Attack(){
 
         isAttacking = true;
         enemySigth.SetActive(false);
-        //float lookingAt = this.transform.rotation.y > 160 ? -1f : 1f;
 
         float elapsedTime = 0f;
-        UnityEngine.Vector3 attackDirection = new UnityEngine.Vector3(0, 1f, 0);
-        UnityEngine.Vector3 targetPosition = transform.position + (attackDirection * jumpDistance);
+        Vector3 attackDirection = new Vector3(0, 1f, 0);
+        Vector3 targetPosition = transform.position + (attackDirection * jumpDistance);
         while (elapsedTime < jumpTime){
 
-            transform.position = UnityEngine.Vector3.Lerp(transform.position, targetPosition, (elapsedTime / jumpTime));
+            transform.position = Vector3.Lerp(transform.position, targetPosition, (elapsedTime / jumpTime));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         elapsedTime = 0f;
-        UnityEngine.Vector3 diveDirection = player.position;
+        Vector3 diveDirection = player.position;
         while (transform.position.y > player.position.y + 1f){
 
-            transform.position = UnityEngine.Vector3.Lerp(transform.position, diveDirection, (elapsedTime / diveTime));
+            transform.position = Vector3.Lerp(transform.position, diveDirection, (elapsedTime / diveTime));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
@@ -46,5 +42,7 @@ public class EnemyMerg : BaseEnemy
         print("EnemyMerg Attack End");
     }
 }
+
+
 
 
